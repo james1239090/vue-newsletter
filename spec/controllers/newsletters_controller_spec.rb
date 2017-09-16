eba6381a7b0171d078ba1d5fffed474fa3b263a2
James <james1239090@gmail.com>
2017-09-16 13:16:25 +1000

    separate test for One assertion per test

--- spec/controllers/newsletters_controller_spec.rb
+++ spec/controllers/newsletters_controller_spec.rb
@@ -2,13 +2,20 @@ require 'rails_helper'
 
 RSpec.describe NewslettersController, type: :controller do
   describe "GET index" do
-    it "assigns @newsletters and render template" do
+    it "assigns @newsletters" do
       newsletter1 = create(:newsletter)
       newsletter2 = create(:newsletter)
 
       get :index
 
       expect(assigns[:newsletters]).to eq([newsletter1, newsletter2])
+
+    end
+
+    it 'render template' do
+      newsletter1 = create(:newsletter)
+      newsletter2 = create(:newsletter)
+      get :index
       expect(response).to render_template("index")
     end
   end
