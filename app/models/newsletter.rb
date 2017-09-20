a5acd403c04840213b204748888e577acd1d99f9
James <james1239090@gmail.com>
2017-09-20 18:24:38 +1000

    add backend validates of mail_to_list mail_cc_list mail_bcc_list for email format

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -2,6 +2,10 @@ class Newsletter < ApplicationRecord
   validates :subject, presence: true
   validates :content, presence: true
 
+  validates :mail_to_list, presence: true,email: true
+  validates :mail_cc_list ,email: true
+  validates :mail_bcc_list ,email: true
+
   serialize :mail_to_list, Array
   serialize :mail_cc_list, Array
   serialize :mail_bcc_list, Array
